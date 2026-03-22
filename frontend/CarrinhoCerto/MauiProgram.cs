a62dbfb932cdf0d630594beb80091e1512a6c6bc
luisboff <luisfernandoboff@gmail.com>
2026-03-22 14:56:56 -0300

    colour palette defined and login page created

--- frontend/CarrinhoCerto/MauiProgram.cs
+++ frontend/CarrinhoCerto/MauiProgram.cs
@@ -13,7 +13,11 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemiBold");
+				fonts.AddFont("Poppins-Bold.ttf", "PoppinsBold");
+				fonts.AddFont("Poppins-Black.ttf", "PoppinsBlack");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
--- frontend/CarrinhoCerto/Pages/LoginPage.xaml.cs
+++ frontend/CarrinhoCerto/Pages/LoginPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Entrar_Clicked(object sender, EventArgs e)
+    {
+        this.Window.Page = new TabNav();
+    }
+
+    private async void TapCadastro_Tapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Cadastro", "O fluxo de cadastro será construído em breve.", "OK");
+    }
+
+    private async void TapEsqueceu_Tapped(object sender, TappedEventArgs e)
+    {
+        await DisplayAlert("Recuperar Senha", "O fluxo de recuperação será construído em breve.", "OK");
+    }
 }
