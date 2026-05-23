4bcac1e7fdc2a5052723901d2ed792beaa46c98d
luisboff <luisfernandoboff@gmail.com>
2026-05-23 18:55:14 -0300

    qr scanner is reading and working

--- frontend/CarrinhoCerto/Pages/Scan/ScanPage.xaml.cs
+++ frontend/CarrinhoCerto/Pages/Scan/ScanPage.xaml.cs
@@ -1,4 +1,5 @@
 using CarrinhoCerto.Pages.Scan;
+using CarrinhoCerto.Pages.Account;
 using ZXing;
 using ZXing.Net.Maui;
 using ZXing.Net.Maui.Readers;
@@ -27,9 +28,9 @@ public partial class ScanPage : ContentPage
             return;
 
         //debug
-        Dispatcher.DispatchAsync(async () =>
+        Dispatcher.DispatchAsync(() =>
         {
-            await DisplayAlert("QR Code Detectado", $"Valor: {primeiro.Value}", "OK");
+            this.Window.Page = new PosScanPage();
         });
     }
 
